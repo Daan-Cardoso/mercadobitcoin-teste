b647b52ce5769ead5be573fc625cd79e3ec794a1
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-13 16:04:12 -0300

    feat: include JSDOC comment on filteredAtrrs function

--- src/helpers/filterAttributes.js
+++ src/helpers/filterAttributes.js
@@ -1,3 +1,21 @@
+/**
+ * Filtra os atributos de um objeto, removendo os atributos que não são aceitos.
+ *
+ * @param {Object} attrs - Objeto contendo os atributos a serem filtrados.
+ * @param {Array<string>} [nonAcceptedAttrs=['class']] - Array contendo as chaves dos atributos que devem ser removidos.
+ * @returns {Object} Novo objeto contendo apenas os atributos que não estão na lista de atributos não aceitos.
+ *
+ * @example
+ * const attrs = {
+ *   class: 'button',
+ *   type: 'text',
+ *   placeholder: 'Enter your name',
+ *   disabled: false
+ * };
+ * 
+ * const result = filteredAttrs(attrs, ['class', 'disabled']);
+ * // result será { type: 'text', placeholder: 'Enter your name' }
+ */
 export const filteredAttrs = (attrs, nonAcceptedAttrs = ['class']) => {
   return Object.fromEntries(
     Object.entries(attrs).filter(([key]) => !nonAcceptedAttrs.includes(key))
